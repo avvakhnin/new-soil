926e7a0bcb33f2a9b55ff51472b4c923bfa0515b
Anton V. Vakhnin <a.v.vakhnin@rambler.ru>
2025-09-24 20:45:45 +0300

    Add beam ray for weapon

--- level/gen_scene/control.gd
+++ level/gen_scene/control.gd
@@ -17,6 +17,8 @@ const ROTATE_SPEED = 1.
 var is_attack = false
 
 func _ready() -> void:
+	for weapon:int in range($Skeleton/BoneAttachment3D.get_child_count()):
+		$Skeleton/BoneAttachment3D.get_child(weapon).hide()
 	switch_weapon()
 
 func _process(delta: float):
@@ -24,16 +26,16 @@ func _process(delta: float):
 		$Camera3D.move_close()
 	if Input.is_action_just_released("close_view"):
 		$Camera3D.move_far()
-	
+
 	if Input.is_action_just_pressed("create_template"):
 		var template: Node3D = load("res://props/template/building_template.tscn").instantiate()
 		get_parent().add_child(template)
 		template.global_position = global_position + (-basis.z) * 10
 		template.global_rotation = global_rotation
-		
+	 
 	if Input.is_action_just_pressed("switch") && mode != CHOP:
 		switch_weapon()
-		
+
 	var prev_mode = mode
 	mode = check_mode()
 	$AnimationPlayer.play(get_animation(mode, curent_weapon_num))
@@ -43,12 +45,10 @@ func _process(delta: float):
 		var speed = SPEED if mode == WALK else RUN_SPEED	
 		var move_distance = Input.get_axis("back", "forward") * speed * delta	
 		transform  = transform.translated_local(move_distance)
-		
-	
+
 	if mode == WALK || mode == RUN || mode == IDLE:
 		var rotate_angle = Input.get_axis("left", "right") * ROTATE_SPEED * delta
 		transform = transform.rotated_local(Vector3.DOWN, rotate_angle)
-
 
 func check_mode() :
 	if mode == CHOP:
@@ -76,10 +76,9 @@ func register_finish_chop():
 		if angle > 0.75: continue
 		template.raise_progress(0.3)
 		break
-		
-		
+
 var resource_spots: Array[ResourceSpot]
-		
+
 var templates: Array[BuildingTemplate]
 
 func _on_area_3d_area_entered(area: Area3D) -> void:	
@@ -89,13 +88,12 @@ func _on_area_3d_area_entered(area: Area3D) -> void:
 		$Backpack.add_storage((handler as ResourceItem).get_storage())
 		handler.queue_free()
 	if handler is BuildingTemplate: templates.append(handler)
-	
 
 func _on_area_3d_area_exited(area: Area3D) -> void:
 	var handler = area.get_parent()
 	if handler is ResourceSpot: resource_spots.erase(handler)
 	if handler is BuildingTemplate: templates.erase(handler)
-	
+
 var curent_weapon_num = 0
 func switch_weapon():
 	$Skeleton/BoneAttachment3D.get_child(curent_weapon_num).hide()
@@ -103,7 +101,7 @@ func switch_weapon():
 	if curent_weapon_num == $Skeleton/BoneAttachment3D.get_child_count():
 		curent_weapon_num = 0
 	$Skeleton/BoneAttachment3D.get_child(curent_weapon_num).show()
-	
+
 func get_animation(mode: int, weapon_num: int) -> String:
 	if mode != CHOP:
 		return mode_animations[mode]
